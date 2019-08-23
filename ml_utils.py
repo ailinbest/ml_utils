7dc1e83ba210a93fd8433f803f3ca1e8e8a326a8
ailin.ren <ailin.ren@bangcle.com>
2019-08-23 17:29:11 +0800

    modify plot_learning_curve

--- ml_utils.py
+++ ml_utils.py
@@ -114,61 +114,89 @@ def plot_pie(data_series, title=None, startangle=0, pctdistance=0.7,
     plt.show()
 
 
-def plot_learning_curve(estimator, X, y, title='学习曲线', ylim=None, cv=None, n_jobs=-1,
-                        train_sizes=np.linspace(.05, 1., 20), verbose=0, plot=True):
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
     """
-    train_size:X 中用来作样本的比例，如果是0.1，则0.1*X用来作全部样本（包括训练集和测试集）
-    cv：用于交叉验证的参数，cv=5代表把X随机分为5份，其中四个作为训练集，一个作为测试集
+    Generate a simple plot of the test and training learning curve.
 
-    画出data在某模型上的learning curve.
-    参数解释
+    Parameters
     ----------
-    estimator : 你用的分类器。
-    title : 表格的标题。
-    X : 输入的feature，numpy类型
-    y : 输入的target vector
-    ylim : tuple格式的(ymin, ymax), 设定图像中纵坐标的最低点和最高点
-    cv : 做cross-validation的时候，数据分成的份数，其中一份作为cv集，其余n-1份作为training(默认为3份)
-    n_jobs : 并行的的任务数(默认1)
-    """
-    train_sizes, train_scores, test_scores = learning_curve(
-        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, verbose=verbose)
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
 
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymax, ymin), optional
+        Defines maximum and minimum yvalues plotated.
+
+    cv : int, cross-validation generator or an iterable, optional
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+          - None, to use the default 3-fold cross-validation,
+          - integer, to specify the number of folds.
+          - :term:`CV splitter`,
+          - An iterable yielding (train, test) splits as arrays of indices.
+
+        For integer/None inputs, if ``y`` is binary or multiclass,
+        :class:`StratifiedKFold` used. If the estimator is not a classifier
+        or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
+
+        Refer :ref:`User Guide <cross_validation>` for the various
+        cross-validators that can be used here.
+
+    n_jobs : int or None, optional (default=None)
+        Number of jobs to run in parallel.
+        ``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
+        ``-1`` means using all processors. See :term:`Glossary <n_jobs>`
+        for more details.
+        使用n_jobs=-1的时候，有问题无法得到结果，使用默认的n_jobs=-1
+
+    train_sizes : array-like, shape (n_ticks,), dtype float or int
+        Relative or absolute numbers of training examples that will be used to
+        generate the learning curve. If the dtype is float, it is regarded as a
+        fraction of the maximum size of the training set (that is determined
+        by the selected validation method), i.e. it has to be within (0, 1].
+        Otherwise it is interpreted as absolute sizes of the training sets.
+        Note that for classification the number of samples usually have to
+        be big enough to contain at least one sample from each class.
+        (default: np.linspace(0.1, 1.0, 5))
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
     test_scores_mean = np.mean(test_scores, axis=1)
     test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+    plt.gca().invert_yaxis()
 
-    if plot:
-        plt.figure()
-        plt.title(title)
-        if ylim is not None:
-            plt.ylim(*ylim)
-        plt.xlabel(u"训练样本数")
-        plt.ylabel(u"得分")
-        plt.gca().invert_yaxis()
-        plt.grid()
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
 
-        plt.fill_between(train_sizes, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std,
-                         alpha=0.1, color="b")
-        plt.fill_between(train_sizes, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std,
-                         alpha=0.1, color="r")
-        plt.plot(train_sizes, train_scores_mean,
-                 'o-', color="b", label=u"训练集上得分")
-        plt.plot(train_sizes, test_scores_mean,
-                 'o-', color="r", label=u"交叉验证集上得分")
-
-        plt.legend(loc="best")
-
-        plt.draw()
-        plt.gca().invert_yaxis()
-        plt.show()
-
-    midpoint = ((train_scores_mean[-1] + train_scores_std[-1]) +
-                (test_scores_mean[-1] - test_scores_std[-1])) / 2
-    diff = (train_scores_mean[-1] + train_scores_std[-1]
-            ) - (test_scores_mean[-1] - test_scores_std[-1])
-    return midpoint, diff
+    plt.legend(loc="best")
+    return plt
 
 
 def plot_confusion_matrix(y_test, y_predict, classes,
